e4f8737f42b6eb5d4a4879e3c79962113e3df30d
adrianH0498 <202011420@urp.edu.pe>
2024-08-03 05:46:53 -0500

    refactor: Remove unused router and update i18n configuration
    This commit removes the unused router import and configuration from the main.js file. Additionally, it updates the i18n configuration to reflect the removal of the router.
    
    The commit message follows the established convention of using a prefix to indicate the type of change ("refactor" for code refactoring) and provides a clear and concise summary of the changes made.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import i18n from './i18n';
 
-createApp(App).use(router).use(i18n).mount('#app')
+createApp(App).use(i18n).mount('#app')
